3b656ee5347f41b5e97804e362123c21f50a6d96
Amanda <amanda.dev.za@gmail.com>
2026-06-02 23:21:13 +0200

    feat: add SnsConfiguration and appsettings structure

---
+++ BankingService/Configuration/SnsConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace BankingService.Configuration
+{
+    public class SnsConfiguration
+    {
+        public string Region { get; set; }
+        public string TopicArn { get; set; }
+    }
+}
